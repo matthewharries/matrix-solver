708f91cbb9e8f9bb7b34cb13358eb7b7163a12f3
Matthew Harries <mharries@simplenexus.com>
2018-07-03 13:58:11 -0600

    Initial Commit

---
+++ MatrixSolver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatrixSolver
+//
+//  Created by Matthew Harries on 7/3/18.
+//  Copyright © 2018 Matthew Harries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
